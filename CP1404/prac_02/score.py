5c3f8951b75e43bccdc7b713f8a555777b6f1645
daniel.mcmahon <daniel.mcmahon1@my.jcu.edu.au>
2022-09-25 18:40:17 +1000

    Refactor Broken Score program to include functions

--- CP1404/prac_02/score.py
+++ CP1404/prac_02/score.py
@@ -3,14 +3,33 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score < 50:
-    print("Bad")
-elif score < 90:
-    print("Passable")
-else:
-    print("Excellent")
+import random
+
+
+def main():
+    """Receive a Score, Evaluate the Score and Generates a Random Score"""
+    score = float(input("Enter score: "))
+    result = evaluate_score(score)
+    print(f"The score {score} is {result}.")
+    random_score = random.randint(0, 100)
+    random_result = evaluate_score(random_score)
+    print(f"The score {random_score} is {random_result}.")
+
+
+def evaluate_score(score):
+    """Return a Score Evaluation"""
+    if score < 0 or score > 100:
+        return "Invalid"
+    elif score < 50:
+        return "Bad"
+    elif score < 90:
+        return "Passable"
+    else:
+        return "Excellent"
+
+
+main()
+
+
 
 
