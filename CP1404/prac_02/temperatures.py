9fb044faee846fc8a2386f72ae848dab3af29bca
daniel.mcmahon <daniel.mcmahon1@my.jcu.edu.au>
2022-09-25 18:23:25 +1000

    Refactor temperatures program to include functions

---
+++ CP1404/prac_02/temperatures.py
@@ -0,0 +1,40 @@
+"""
+CP1404/CP5632 - Practical
+Pseudocode for temperature conversion
+"""
+
+MENU = "C - Convert Celsius to Fahrenheit\n" \
+       "F - Convert Fahrenheit to Celsius\n" \
+       "Q - Quit"
+
+
+def main():
+    """Function docstring"""
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "C":
+            celsius = float(input("Celsius: "))
+            fahrenheit = calculate_celsius(celsius)
+            print("Result: {:.2f} F".format(fahrenheit))
+        elif choice == "F":
+            fahrenheit = float(input("Fahrenheit: "))
+            celsius = calculate_fahrenheit(fahrenheit)
+            print(f"Result: {celsius:.2f} C")
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+
+def calculate_celsius(celsius):
+    return celsius * 9.0 / 5 + 32
+
+
+def calculate_fahrenheit(fahrenheit):
+    return (fahrenheit - 32) * 5 / 9
+
+
+main()
+
