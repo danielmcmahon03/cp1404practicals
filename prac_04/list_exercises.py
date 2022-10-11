0d088f73c9fee073e2cb345917c7bebe26ab1911
daniel.mcmahon <daniel.mcmahon1@my.jcu.edu.au>
2022-10-11 14:17:06 +1000

    Add file to prac03 folder

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,12 @@
+"""
+CP1404 - Practical 4
+List Exercises
+"""
+
+# Basic List Operations
+
+numbers = []
+for number in range(0, 5):
+    number = int(input("Number: "))
+    numbers.append(number)
+print(numbers)
