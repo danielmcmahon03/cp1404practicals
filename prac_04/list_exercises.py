e4956cbc02c258881025984f61a3950282d1b927
daniel.mcmahon <daniel.mcmahon1@my.jcu.edu.au>
2022-10-11 14:25:10 +1000

    Add file to prac04 folder

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -6,7 +6,24 @@ List Exercises
 # Basic List Operations
 
 numbers = []
-for number in range(0, 5):
+for i in range(0, 5):
     number = int(input("Number: "))
     numbers.append(number)
-print(numbers)
+print(f"The first number is {numbers[0]}")
+print(f"The last number is {numbers[4]}")
+print(f"The smallest number is {min(numbers)}")
+print(f"The largest number is {max(numbers)}")
+print(f"The average of the numbers is {sum(numbers)/len(numbers)}")
+
+# Security Checker
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45',
+             'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState',
+             'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+username = input("Username: ")
+if username in usernames:
+    print("Access Granted")
+else:
+    print("Access Denied")
+
+
