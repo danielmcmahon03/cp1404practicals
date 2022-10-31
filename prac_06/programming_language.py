2e9e185682d2476699ec327932d806c4081d58c5
daniel.mcmahon <daniel.mcmahon1@my.jcu.edu.au>
2022-10-31 22:05:16 +1000

    Update class to include str and repr method.

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -2,4 +2,23 @@
 CP1404 - Programming Language Class
 """
 
+
 class ProgrammingLanguage:
+    """."""
+
+    def __init__(self, field, typing, reflection, year):
+        """."""
+        self.field = field
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def __str__(self):
+        return f"{self.field}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+
+    def __repr__(self):
+        return f"{self.field} {self.typing} {self.reflection} {self.year}"
+
+    def is_dynamic(self):
+        if self.typing == "Dynamic":
+            return f"{self.field}"
