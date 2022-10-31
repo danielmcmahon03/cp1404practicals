5bc76ad9f60075c0a2cfa3d8d7625c57202c5afd
daniel.mcmahon <daniel.mcmahon1@my.jcu.edu.au>
2022-10-31 22:05:57 +1000

    Complete practical 6 languages exercise.

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -1,6 +1,21 @@
 """
 Languages
 Estimate: 20 minutes
-Actual:   xx minutes
+Actual:   22 minutes
 """
 
+from prac_06.programming_language import ProgrammingLanguage
+
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+print(python)
+print(ruby)
+
+programming_languages = [python, ruby, visual_basic]
+print(programming_languages)
+
+print("The dynamically typed languages are:")
+for programming_language in programming_languages:
+    if programming_language.is_dynamic():
+        print(programming_language.is_dynamic())
