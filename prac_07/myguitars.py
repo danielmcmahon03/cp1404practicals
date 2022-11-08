02f96bce00ca60770632339d21c170b115e3c8dc
daniel.mcmahon <daniel.mcmahon1@my.jcu.edu.au>
2022-11-09 00:12:14 +1000

    Complete myguitars exericse.

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -3,29 +3,32 @@ CP1404 - Practical 7
 My Guitars
 """
 
-import csv
-# from collections import namedtuple
-# from language_file_reader import using_csv
-
-
 from prac_06.guitar import Guitar
+
+FILENAME = "guitars.csv"
 
 
 def main():
-    """."""
+    """Read guitar.csv file, display the file contents, add new guitars and save them to a file."""
     guitars = []
-    in_file = open('guitars.csv', 'r')
-    in_file.readline()
+    in_file = open(FILENAME, 'r')
     for line in in_file:
         parts = line.strip().split(',')
-        guitar = Guitar(parts[0], parts[1], float(parts[2]))
+        guitar = Guitar(parts[0], int(parts[1]), float(parts[2]))
         guitars.append(guitar)
     in_file.close()
+    display_guitars(guitars)
+    add_guitar(guitars)
+    save_guitars(guitars)
+
+    display_guitars(guitars)
+
+
+def display_guitars(guitars):
+    """Display guitars in list."""
     guitars.sort()
     for guitar in guitars:
         print(guitar)
-
-    add_guitar(guitars)
 
 
 def add_guitar(guitars):
@@ -38,6 +41,14 @@ def add_guitar(guitars):
         guitar = Guitar(name, year, cost)
         guitars.append(guitar)
         name = input("Name: ")
+
+
+def save_guitars(guitars):
+    """Write guitars to an outfile"""
+    with open(FILENAME, "w") as out_file:
+        for guitar in guitars:
+            print(f"{guitar.name},{guitar.year},{guitar.cost}",
+                  file=out_file)
 
 
 main()
