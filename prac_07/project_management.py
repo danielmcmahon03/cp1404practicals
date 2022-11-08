7579672c69e6d693556aa55bc111a2fdc9d31201
daniel.mcmahon <daniel.mcmahon1@my.jcu.edu.au>
2022-11-09 09:25:14 +1000

    Initial commit.

---
+++ prac_07/project_management.py
@@ -0,0 +1,29 @@
+"""
+CP1404 - Practical 7
+Project Management
+Estimate: 45 minutes
+Time:
+"""
+
+MENU = "(L)oad projects\n(S)ave projects\n" \
+       "(D)isplay projects\n(F)ilter projects by date\n" \
+       "(A)dd new project\n(U)pdate project\n(Q)uit"
+
+print(MENU)
+
+
+# display menu
+# get choice
+# while choice != <quit option>
+#     if choice == <first option>
+#         <do first task>
+#     else if choice == <second option>
+#         <do second task>
+#     ...
+#     else if choice == <n-th option>
+#         <do n-th task>
+#     else
+#         display invalid input error message
+#     display menu
+#     get choice
+# <do final thing, if needed>
