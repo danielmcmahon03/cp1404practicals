bd55b10a40fdb3187235478c89bfce5ba145ac4d
daniel.mcmahon <daniel.mcmahon1@my.jcu.edu.au>
2022-11-09 10:34:13 +1000

    Update double under methods.

--- prac_07/project.py
+++ prac_07/project.py
@@ -14,7 +14,13 @@ class Project:
 
     def __repr__(self):
         return f"{self.name}, start: {self.start_date}, priority {self.priority}, " \
-               f"estimate: ${self.cost}, completion: {self.completion}%"
+               f"estimate: ${self.cost:,.2f}, completion: {self.completion}%"
 
     def __lt__(self, other):
         return self.priority < other.priority
+
+    def __gt__(self, other):
+        return self.start_date < other.start_date
+
+    def is_complete(self):
+        return self.completion == "100"
