267e80be4928b42f5b43543bdd91fd32099b9d53
daniel.mcmahon <daniel.mcmahon1@my.jcu.edu.au>
2022-11-17 15:57:25 +1000

    Update first part of Test Texi.

---
+++ prac_09/taxi_test.py
@@ -0,0 +1,13 @@
+"""
+Practical 9 - Taxi Test
+"""
+
+from taxi import Taxi
+
+my_taxi = Taxi("Prius 1", 100, 1.23)
+my_taxi.drive(40)
+print(f"{my_taxi} is: ${my_taxi.get_fare():.2f}.")
+
+my_taxi.start_fare()
+my_taxi.drive(100)
+print(f"{my_taxi} is: ${my_taxi.get_fare():.2f}.")
